8835ae2c38fe7b68f001cf3ccdf253e88353f6d5
Michael Meisel <mrmeisel@gmail.com>
2020-05-17 18:45:52 -0700

    Second exercise

---
+++ 02-first-light/02-first-light.ino
@@ -0,0 +1,36 @@
+// Slightly modified version of the FastLED FirstLight example:
+// https://github.com/FastLED/FastLED/blob/master/examples/FirstLight/FirstLight.ino
+#include <FastLED.h>
+
+// How many leds are in the strip?
+#define NUM_LEDS 60
+
+// Which pin is the data line connected to?
+#define DATA_PIN 7
+
+// This is an array of leds.  One item for each led in your strip.
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // Set up the LEDs and tell the controller about them
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+}
+
+void loop() {
+  // Move a single white led
+  for (int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+    // Turn our current led on to white
+    // See https://github.com/FastLED/FastLED/wiki/Pixel-reference for available colors and more
+    leds[whiteLed] = CRGB::White;
+
+    // Show the leds (only one of which is set to white, from above)
+    FastLED.show();
+
+    // Wait a little bit
+    delay(100);
+
+    // Turn our current led back to black for the next loop around
+    leds[whiteLed] = CRGB::Black;
+  }
+}
